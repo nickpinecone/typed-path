2947fbafc6e64acaa7bb0a00f758486cf5306a16
nickpinecone <nick.pineconefir@gmail.com>
2025-08-02 22:29:27 +0500

    feat: initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using TypedPath;
+
+namespace Test;
+
+[TypedPath("Assets")]
+public static partial class Assets
+{
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
