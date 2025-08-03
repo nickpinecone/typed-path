eb345eb630425975af96abeb8b900b2a0e5bee2f
nickpinecone <nick.pineconefir@gmail.com>
2025-08-03 19:48:58 +0500

    feat: scan files with AdditionalFiles approach

--- SourceGen/SourceHelper.cs
+++ SourceGen/SourceHelper.cs
@@ -1,3 +1,7 @@
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
 namespace SourceGen;
 
 public static class SourceHelper
@@ -18,15 +22,61 @@ public static class SourceHelper
         }
         """;
 
-    public static string GenerateClass(string name)
+    public static string GenerateClass(string name, ImmutableArray<AdditionalText> files)
     {
-        return $@"
-namespace Sample;
+        var builder = new StringBuilder();
+        builder.AppendLine("namespace Sample;");
+        builder.AppendLine($"public static partial class {name}");
+        builder.AppendLine("{");
 
-public static partial class {name}
-{{
-    public const string CoolImage = ""/somepath"";
-}}
-               ";
+        foreach (var file in files)
+        {
+            var filename = Path.GetFileNameWithoutExtension(file.Path).ToPascalCase();
+            var split = file.Path.Split([name], StringSplitOptions.None);
+            var relative = $"{name}{split.Last()}";
+
+            builder.AppendLine($"   public const string {filename} = \"{relative}\";");
+        }
+
+        builder.AppendLine("}");
+
+        return builder.ToString();
+    }
+
+    public static string ToPascalCase(this string text)
+    {
+        var builder = new StringBuilder();
+        var flag = true;
+
+        for (var index = 0; index < text.Length; ++index)
+        {
+            var c = text[index];
+
+            if (char.IsLetterOrDigit(c))
+            {
+                if (flag)
+                {
+                    builder.Append(char.ToUpperInvariant(c));
+                    flag = false;
+                }
+                else
+                {
+                    builder.Append(index >= text.Length - 1 || !char.IsUpper(c) || !char.IsLower(text[index + 1])
+                        ? char.ToLowerInvariant(c)
+                        : c);
+                }
+            }
+            else
+            {
+                flag = true;
+            }
+
+            if (index < text.Length - 1 && char.IsLower(text[index]) && char.IsUpper(text[index + 1]))
+            {
+                flag = true;
+            }
+        }
+
+        return builder.ToString();
     }
 }
