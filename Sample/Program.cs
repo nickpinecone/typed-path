648337f1a31738f3af959ffd364c5b83680b61a6
nickpinecone <nick.pineconefir@gmail.com>
2025-08-04 18:28:44 +0500

    fix: nested folder path

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,10 +7,16 @@ public static partial class Assets
 {
 }
 
+[TypedPath("Assets/SubFolder")]
+public static partial class SubFolder
+{
+}
+
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine(Assets.SubFolder.NestedFolder.SuperNested);
+        Console.WriteLine(SubFolder.NestedFolder.SuperNested);
     }
 }
