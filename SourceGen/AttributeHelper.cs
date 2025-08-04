7881ca784551a54558e2f2bb8d86f6fc44ac2428
nickpinecone <nick.pineconefir@gmail.com>
2025-08-04 10:27:52 +0500

    feat: get attribute path and namespace

---
+++ SourceGen/AttributeHelper.cs
@@ -0,0 +1,33 @@
+using Microsoft.CodeAnalysis;
+
+namespace SourceGen;
+
+public static class AttributeHelper
+{
+    public static string? GetAttributeArgumentValue(this AttributeData attribute, string argumentName)
+    {
+        foreach (var namedArg in attribute.NamedArguments)
+        {
+            if (namedArg.Key == argumentName)
+            {
+                return namedArg.Value.Value?.ToString();
+            }
+        }
+
+        var constructorParams = attribute.AttributeConstructor?.Parameters;
+
+        if (constructorParams is not null)
+        {
+            for (var i = 0; i < constructorParams.Value.Length; i++)
+            {
+                if (constructorParams.Value[i].Name == argumentName.ToLower() &&
+                    i < attribute.ConstructorArguments.Length)
+                {
+                    return attribute.ConstructorArguments[i].Value?.ToString();
+                }
+            }
+        }
+
+        return null;
+    }
+}
